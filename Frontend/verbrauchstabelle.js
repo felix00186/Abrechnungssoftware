3619b2f0667c52d677f82807ffd804900c9794e2
Felix Semt <felix.semt@tu-ilmenau.de>
2025-04-27 22:19:47 +0200

    Spalten für Artikeltabelle implementiert

--- Frontend/verbrauchstabelle.js
+++ Frontend/verbrauchstabelle.js
@@ -1,17 +1,54 @@
-class Verbrauchstabelle {
+class ArtikeltabelleColumns {
 
-    constructor(artikel_list, oeffnung_list, oeffnung_id, parent) {
+    constructor() {
+        this.list = [];
+    }
+
+    addNameColumn(title='Name') {
+        this.list.push({type: 'name', title: title});
+    }
+
+    addLosgroesseColumn(title='Losgröße') {
+        this.list.push({type: 'losgroesse', title: title});
+    }
+
+    addValueColumn(title='Anzahl', positive=true) {
+        this.list.push({type: 'value', title: title, factor: positive ? 1 : -1});
+    }
+
+    addSumColumn(title, summands) {
+        this.list.push({type: 'sum', title: title, summands: summands});
+    }
+
+    addCostColumn(title, amount) {
+        this.list.push({type: 'cost', title: title, amount: amount});
+    }
+
+    addNumberColumn(title) {
+        this.list.push({type: 'number', title: title});
+    }
+
+    addDeleteColumn() {
+        this.list.push({type: 'delete', title: '&#x1F5D1;&#xFE0F;'});
+    }
+}
+
+
+class Artikeltabelle {
+
+    constructor(columns, artikel_list, oeffnung_list, oeffnung_id, parent) {
         this.artikel_list = artikel_list;
         this.oeffnung_list = oeffnung_list;
         this.oeffnung_id = oeffnung_id;
+        this.columns = columns.list;
         this.parent = parent;
+        this.articles = {};
         if (oeffnung_id == null) {
             this.verbauch_list = [];
         } else {
             get_json('/func/oeffnungen/artikel', {'id': oeffnung_id})
                 .then(resp => {this.verbrauch_list = resp;});
         }
-        this.createTable();
     }
 
     loadOeffnung() {
@@ -19,7 +56,27 @@ class Verbrauchstabelle {
     }
 
     createTable() {
-
+        this.output = document.createElement('table');
+        this.output.className = 'standard';
+        this.title_row = document.createElement('tr');
+        this.output.appendChild(this.title_row);
+        this.columns.forEach(col_scheme => {
+            const col = document.createElement('th');
+            this.title_row.appendChild(col);
+            col.innerHTML = col_scheme.title;
+        });
+        this.parent.appendChild(this.output);
     }
 
+    addLine(id, ...values) {
+        if (id in this.articles) {
+
+        } else {
+            
+        }
+    }
+
+    renderArticle(id) {
+
+    }
 }
