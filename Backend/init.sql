0bcf89bb083d282a6b95719dddb70d9cef2abf9a
Felix Semt <felix.semt@tu-ilmenau.de>
2025-12-02 19:00:46 +0100

    Syntax in SQL korrigiert

--- Backend/init.sql
+++ Backend/init.sql
@@ -11,13 +11,13 @@ create table EinkaufArtikel
 (
     einkauf         INTEGER not null                    references Einkaeufe(id),
     getraenk        INTEGER not null                    references Getraenke(id),
-    menge           REAL    not null    default 0                                           check menge > 0,
-    gesamt_netto    INTEGER not null    default 0                                           check gesamt_netto >= 0,
-    einzel_netto    INTEGER not null    default 0                                           check einzel_netto >= 0,
-    mwst            REAL    not null    default 0                                           check mwst >= 0,
-    gesamt_brutto   INTEGER not null    default 0                                           check gesamt_brutto >= 0,
+    menge           REAL    not null    default 0                                           check (menge > 0),
+    gesamt_netto    INTEGER not null    default 0                                           check (gesamt_netto >= 0),
+    einzel_netto    INTEGER not null    default 0                                           check (einzel_netto >= 0),
+    mwst            REAL    not null    default 0                                           check (mwst >= 0),
+    gesamt_brutto   INTEGER not null    default 0                                           check (gesamt_brutto >= 0),
     losgroesse      INTEGER             default null    references Losgroessen(id),
-    orig_menge      REAL    not null    default 0                                           check orig_menge >= 0,
+    orig_menge      REAL    not null    default 0                                           check (orig_menge >= 0),
     metadata        TEXT                default null
 );
 
@@ -25,18 +25,18 @@ create table EinkaufBereich
 (
     einkauf         INTEGER not null                    references Einkaeufe(id),
     bereich         INTEGER not null                    references Oeffnungsarten(id),
-    netto           INTEGER not null    default 0                                           check netto >= 0,
-    mwst            REAL    not null    default 0                                           check mwst >= 0,
-    brutto          INTEGER not null    default 0                                           check brutto >= 0
+    netto           INTEGER not null    default 0                                           check (netto >= 0),
+    mwst            REAL    not null    default 0                                           check (mwst >= 0),
+    brutto          INTEGER not null    default 0                                           check (brutto >= 0)
 );
 
 create table EinkaufOeffnung
 (
     einkauf         INTEGER not null                    references Einkaeufe(id),
     oeffnung        INTEGER not null                    references Oeffnungen(id),
-    netto           INTEGER not null    default 0                                           check netto >= 0,
-    mwst            REAL    not null    default 0                                           check mwst >= 0,
-    brutto          INTEGER not null    default 0                                           check brutto >= 0
+    netto           INTEGER not null    default 0                                           check (netto >= 0),
+    mwst            REAL    not null    default 0                                           check (mwst >= 0),
+    brutto          INTEGER not null    default 0                                           check (brutto >= 0)
 );
 
 create table Einnahmen
@@ -44,20 +44,20 @@ create table Einnahmen
     id              INTEGER primary key,
     oeffnung        INTEGER not null                    references Oeffnungen(id),
     name            TEXT    not null    default '',
-    brutto          INTEGER not null    default 0                                           check brutto >= 0,
-    mwst            INTEGER not null    default 19                                          check mwst between 0 and 99,
-    zehnt           INTEGER not null    default 10                                          check zehnt between 0 and 99,
-    netto           INTEGER not null    default 0                                           check netto >= 0
+    brutto          INTEGER not null    default 0                                           check (brutto >= 0),
+    mwst            INTEGER not null    default 19                                          check (mwst between 0 and 99),
+    zehnt           INTEGER not null    default 10                                          check (zehnt between 0 and 99),
+    netto           INTEGER not null    default 0                                           check (netto >= 0)
 );
 
 create table Getraenke
 (
     id              INTEGER primary key,
     name            TEXT    not null    default '',
-    einheit_lang    TEXT    not null    default 'Liter'                                     check einheit_lang in ('Liter', 'Centiliter', 'Milliliter', 'Kilogramm', 'Gramm', 'Stück', 'Flaschen', 'Dosen', 'Kannen', 'Packungen', 'Kilometer'),
-    einheit_kurz    TEXT    not null    default 'l'                                         check einheit_kurz in ('l', 'cl', 'ml', 'kg', 'g', 'Stk.', 'Fl.', 'Dos.', 'Kan.', 'Pck.', 'km'),
-    sichtbar        TINYINT not null    default 1                                           check sichtbar in (0, 1),
-    reste           TINYINT not null    default 1                                           check reste in (0, 1),
+    einheit_lang    TEXT    not null    default 'Liter'                                     check (einheit_lang in ('Liter', 'Centiliter', 'Milliliter', 'Kilogramm', 'Gramm', 'Stück', 'Flaschen', 'Dosen', 'Kannen', 'Packungen', 'Kilometer')),
+    einheit_kurz    TEXT    not null    default 'l'                                         check (einheit_kurz in ('l', 'cl', 'ml', 'kg', 'g', 'Stk.', 'Fl.', 'Dos.', 'Kan.', 'Pck.', 'km')),
+    sichtbar        TINYINT not null    default 1                                           check (sichtbar in (0, 1)),
+    reste           TINYINT not null    default 1                                           check (reste in (0, 1)),
     notizen         TEXT    not null    default '',
     bereich         INTEGER             default null    references Oeffnungsarten(id),
     check (einheit_kurz = case einheit_lang when 'Liter'       then 'l'
@@ -78,16 +78,16 @@ create table Kalkulationen
 (
     id              INTEGER primary key,
     name            TEXT    not null    default '',
-    preis           INTEGER not null    default 0                                           check preis >= 0,
+    preis           INTEGER not null    default 0                                           check (preis >= 0),
     notizen         TEXT    not null    default '',
-    anzeigen        TINYINT not null    default 1                                           check anzeigen in (0, 1)
+    anzeigen        TINYINT not null    default 1                                           check (anzeigen in (0, 1))
 );
 
 create table KalkulationenArtikel
 (
     kalk            INTEGER not null                    references Kalkulationen(id),
     artikel         INTEGER not null                    references Getraenke(id),
-    menge           REAL    not null    default 0                                           check menge >= 0
+    menge           REAL    not null    default 0                                           check (menge >= 0)
 );
 
 create table KalkulationenSonstigeArtikel
@@ -95,7 +95,7 @@ create table KalkulationenSonstigeArtikel
     kalk            INTEGER                             references Kalkulationen(id),
     name            TEXT    not null    default '',
     einzelpreis     INTEGER not null    default 0,
-    menge           REAL    not null    default 0                                           check menge >= 0
+    menge           REAL    not null    default 0                                           check (menge >= 0)
 );
 
 create table Losgroessen
@@ -105,8 +105,8 @@ create table Losgroessen
     name            TEXT    not null    default '',
     faktor          REAL    not null    default 0,
     summand         REAL                default 0,
-    sichtbar        TINYINT not null    default 1                                           check sichtbar in (0, 1),
-    check not(faktor = 0 and summand = 0)
+    sichtbar        TINYINT not null    default 1                                           check (sichtbar in (0, 1)),
+    check (not(faktor = 0 and summand = 0))
 );
 
 create table Oeffnungen
@@ -122,20 +122,20 @@ create table OeffnungenVerbrauch
 (
     oeffnung        INTEGER not null                    references Oeffnungen(id),
     getraenk        INTEGER not null                    references Getraenke(id),
-    anfang          REAL    not null    default 0.0                                         check anfang >= 0,
-    plus            REAL    not null    default 0.0                                         check plus >= 0,
-    ende            REAL    not null    default 0.0                                         check ende >= 0,
-    differenz       REAL    not null    default 0.0                                         check differenz >= 0,
+    anfang          REAL    not null    default 0.0                                         check (anfang >= 0),
+    plus            REAL    not null    default 0.0                                         check (plus >= 0),
+    ende            REAL    not null    default 0.0                                         check (ende >= 0),
+    differenz       REAL    not null    default 0.0                                         check (differenz >= 0),
     losgroesse      INTEGER             default null    references Losgroessen(id),
-    differenz_orig  REAL    not null    default 0.0                                         check differenz_orig >= 0,
+    differenz_orig  REAL    not null    default 0.0                                         check (differenz_orig >= 0),
     metadata        TEXT                default null
 );
 
 create table Oeffnungsarten
 (
     id              INTEGER primary key,
-    name            TEXT    not null    default 'Öffnung'                                   check length(name) > 0,
-    sichtbar        TINYINT not null    default 1                                           check sichtbar in (0, 1),
+    name            TEXT    not null    default 'Öffnung'                                   check (length(name) > 0),
+    sichtbar        TINYINT not null    default 1                                           check (sichtbar in (0, 1)),
     notizen         TEXT    not null    default ''
 );
 
@@ -143,7 +143,7 @@ create table OeffnungsartGetraenke
 (
     id              INTEGER not null                    references Oeffnungsarten(id),
     getraenk        INTEGER not null                    references Getraenke(id),
-    nummer          INTEGER not null    default 0                                           check nummer >= 0,
+    nummer          INTEGER not null    default 0                                           check (nummer >= 0),
     losgroesse      INTEGER             default null    references Losgroessen(id)
 );
 
